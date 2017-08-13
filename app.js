cf520e4354755dd8202d02d633c1247f8239baa3
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-08-13 20:31:14 +0100

    add media query

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.render('layout');
 });
